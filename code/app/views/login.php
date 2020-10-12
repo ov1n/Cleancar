9472d737569209100205a51dc53e26bbc6d14b11
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-10-12 17:22:13 +0530

    1st loging form

--- code/app/views/login.php
+++ code/app/views/login.php
@@ -1,12 +1,22 @@
+<!DOCTYPE html>
 <html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewreport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Loging Form</title>
+</head>
+<body>
+	<div class="signup-form">
+		<img src="user-icons-56.PNG">
+		<form action="login.html" method="post">
+			<input type="text" placeholder="User Name" class="txt">
+			<input type="password" placeholder="Password" class="txt">
+			<input type="button" value="Login" class="btn">
+		</form>	
 
-<h1> Login </h1>
-    <body>
-        <form action="" method="post">
-        <label for="POST-name">Name:</label>
-        <input id="POST-name" type="text" name="name">
-        <input type="submit" value="Save">
-        </form>
-    </body>
+	</div>
 
+</body>
 </html>
