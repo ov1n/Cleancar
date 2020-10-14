510ccf61322b94b524548ef3ffd1245211e848e0
ov1n <39676588+ov1n@users.noreply.github.com>
2020-10-14 19:02:58 +0530

    Finalized insert function for customer

--- code/app/models/Customer.php
+++ code/app/models/Customer.php
@@ -7,6 +7,7 @@
         public function __construct(){
                 //$db=new Database();
         }
+
         //checking credentials by searching through the customer table
         function check_credentials($uname,$pwd){
             
@@ -30,6 +31,16 @@
             if($count){
                 echo("go to view");
             }
+        }
+
+        //function to insert data into table customer
+        function insert_record($first_name,$last_name,$vehicle_number,$address,$e_mail,$password,$mobile_tel_no,$home_tel_no){
+            
+            //assign connectivity to a variable
+            $conn=Database::conn();
+
+            $query="INSERT INTO customer(first_name,last_name,vehicle_number,address,e_mail,password,mobile_tel_no,home_tel_no) VALUES($first_name,$last_name,$vehicle_number,$address,$e_mail,$password,$mobile_tel_no,$home_tel_no)";
+
         }
         
     }
