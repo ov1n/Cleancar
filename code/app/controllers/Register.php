51ce75e5abb95ce974d93fe1717073bd7a157c41
ov1n <39676588+ov1n@users.noreply.github.com>
2020-10-14 17:55:46 +0530

    added register controller

--- code/app/controllers/Register.php
+++ code/app/controllers/Register.php
@@ -2,8 +2,21 @@
     class Register extends Controller{
         //base controller just for extending
         //IN CASE OF OVERRIDE
-        //public static function create_view(){
-        //    echo "viewwww";
-        //}
+        function register(){
+
+            $cust=new Customer();
+            
+            //get each field from form
+            $first_name=$_POST['first_name'];
+            $last_name=$_POST['last_name'];
+            $vehicle_number=$_POST['vehicle_number'];
+            $address=$_POST['address'];
+            $e_mail=$_POST['e-mail'];
+            $password=$_POST['password'];
+            $mobile_tel_no=$_POST['mobile_tel_no'];
+            $home_tel_no=$_POST['home_tel_no'];
+
+            $cust->insert_record($first_name,$last_name,$vehicle_number,$address,$e_mail,$password,$mobile_tel_no,$home_tel_no);
+        }
     }
 ?>
