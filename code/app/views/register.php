71f1a6d9bc07eccbc913c6f1425d06fabf3a0625
TharinduWithanage <56493297+TharinduWithanage@users.noreply.github.com>
2020-10-16 13:17:02 +0530

    modified register ui

--- code/app/views/register.php
+++ code/app/views/register.php
@@ -4,11 +4,11 @@
     <head>
         <title>CleanCar - Customer Registration form</title>
 
-        <!-- get font just in case -->
-        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
-        <!-- get css files -->
-        <link rel="stylesheet" type="text/css" href="public\css\register.css">
-        <link rel="stylesheet" type="text/css" href="public\css\footer.css">
+        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet"><!-- get font just in case -->
+
+        <link rel="stylesheet" type="text/css" href="public\css\form.css"><!-- add style to form -->
+        <link rel="stylesheet" type="text/css" href="public\css\nav_bar.css"><!-- add style to nav bar -->
+        <link rel="stylesheet" type="text/css" href="public\css\footer.css"><!-- add style to footer -->
         
         <!-- get imported jquery -->
         <!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> -->
@@ -21,64 +21,65 @@
     </head>
 
     <body>
-      <!-- include footer seperately  -->
       <?php
-                include("nav_login.php");
-            ?>
+        //include nav bar externally
+        include("nav_login.php");
+      ?>
 
-        <ul class="breadcrumb">
-          <img src="public\images\4444.png" width="109" height="48" style="float:right">
-          </br>
-          <li><a href="home">Home</a></li>
-          <li>Customer Registration</li>  
-        </ul>
-        <form method="POST" name="reg_form" id="reg_form" onsubmit="create()">
-            <div class="container">  
-              <h1>Registration</h1>
-              <p>Please fill in this form to register as a customer</p>
-              <div class="background">
-                <hr>
-              <div class="card" >
+      <ul class="breadcrumb">
+        <img src="public\images\4444.png" width="109" height="48" style="float:right">
+        </br>
+        <li><a href="home">Home</a></li>
+        <li>Customer Registration</li>  
+      </ul>
+
+      <form method="POST" name="reg_form" id="reg_form" onsubmit="create()">
+        <div class="container">  
+          <h1>Registration</h1>
+          <p>Please fill in this form to register as a customer</p>
+          <div class="background">
+            <hr>
+            <div class="card" >
               <div id=form_content> <!-- div to center needed elements -->
 
-              <!-- &ensp characters are used to improve clarity and keep clear spacing -->
+                <!-- &ensp characters are used to improve clarity and keep clear spacing -->
               
-              &ensp;&ensp;&ensp;&ensp;<label for="first_name"><div class="form_label"><b>First Name</b></div></label>
-              <input type="text" placeholder="Enter Your First Name" name="first_name" id="first_name" required size="50"></br>
+                &ensp;&ensp;&ensp;&ensp;<label for="first_name"><div class="form_label"><b>First Name</b></div></label>
+                <input type="text" placeholder="Enter Your First Name" name="first_name" id="first_name" required size="50"></br>
 
-              &ensp;&ensp;&ensp;&ensp;<label for="last_name"><div class="form_label"><b>Last Name</b></div></label>
-              <input type="text" placeholder="Enter Your Last Name" name="last_name" id="last_name" required></br>
+                &ensp;&ensp;&ensp;&ensp;<label for="last_name"><div class="form_label"><b>Last Name</b></div></label>
+                <input type="text" placeholder="Enter Your Last Name" name="last_name" id="last_name" required></br>
 
-              &ensp;&ensp;&ensp;&ensp;<label for="vehicle_number"><div class="form_label"><b>Vehicle Number</b></div></label>
-              <input type="text" placeholder="Vehicle Number" name="vehicle_number" id="vehicle_number" required>&ensp;
-              <!-- DELETE BUTTON IF UNNECESSARY -->
-              <button class="btn btn_round" id="add_vehicle_number" formnovalidate>&ensp;+&ensp;</button></br>
+                &ensp;&ensp;&ensp;&ensp;<label for="vehicle_number"><div class="form_label"><b>Vehicle Number</b></div></label>
+                <input type="text" placeholder="Vehicle Number" name="vehicle_number" id="vehicle_number" required>&ensp;
+                <!-- DELETE BUTTON IF UNNECESSARY -->
+                <button class="btn btn_round" id="add_vehicle_number" formnovalidate>&ensp;+&ensp;</button></br>
 
-              &ensp;&ensp;&ensp;&ensp;<label for="address"><div class="form_label"><b>Address</b></div></label>
-              <input type="text" placeholder="Enter Your Address" name="address" id="address" required></br>
+                &ensp;&ensp;&ensp;&ensp;<label for="address"><div class="form_label"><b>Address</b></div></label>
+                <input type="text" placeholder="Enter Your Address" name="address" id="address" required></br>
 
-              &ensp;&ensp;&ensp;&ensp;<label for="email"><div class="form_label"><b>Email</b></div></label>
-              <input type="email" placeholder="Enter Your Email Address" name="email" id="email" required></br>
+                &ensp;&ensp;&ensp;&ensp;<label for="email"><div class="form_label"><b>Email</b></div></label>
+                <input type="email" placeholder="Enter Your Email Address" name="email" id="email" required></br>
 
-              &ensp;&ensp;&ensp;&ensp;<label for="password"><div class="form_label"><b>Password</b></div></label>
-              <input type="password" placeholder="Enter Password" name="password" id="password" onkeyup='confirm_password();button_enable();' required></br>
+                &ensp;&ensp;&ensp;&ensp;<label for="password"><div class="form_label"><b>Password</b></div></label>
+                <input type="password" placeholder="Enter Password" name="password" id="password" onkeyup='confirm_password();button_enable();' required></br>
           
-              &ensp;&ensp;&ensp;&ensp;<label for="psw-repeat"><div class="form_label"><b>Repeat Password</b></div></label>
-              <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" onkeyup='confirm_password();button_enable();' required></br>
-              <div id="psw-message"><p></p></div>
+                &ensp;&ensp;&ensp;&ensp;<label for="psw-repeat"><div class="form_label"><b>Repeat Password</b></div></label>
+                <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" onkeyup='confirm_password();button_enable();' required></br>
+                <div id="psw-message"><p></p></div>
 
 
-              &ensp;&ensp;&ensp;&ensp;<h3>Contact Numbers</h3>
-              &ensp;&ensp;&ensp;&ensp;<label for="mobile_tel_no" class="form_label"><div class="form_label"><b>Mobile</b></div></label>
-              <input type="tel" placeholder="Enter Your mobile number" name="mobile_tel_no" id="mobile_tel_no" required></br>
+                &ensp;&ensp;&ensp;&ensp;<h3>Contact Numbers</h3>
+                &ensp;&ensp;&ensp;&ensp;<label for="mobile_tel_no" class="form_label"><div class="form_label"><b>Mobile</b></div></label>
+                <input type="tel" placeholder="Enter Your mobile number" name="mobile_tel_no" id="mobile_tel_no" required></br>
               
-              &ensp;&ensp;&ensp;&ensp;<label for="home_tel_no" class="form_label"><div class="form_label"><b>Home</b></div></label>
-              <input type="tel" placeholder="Enter Your Home number" name="home_tel_no" id="home_tel_no" required>
-              </br>
+                &ensp;&ensp;&ensp;&ensp;<label for="home_tel_no" class="form_label"><div class="form_label"><b>Home</b></div></label>
+                <input type="tel" placeholder="Enter Your Home number" name="home_tel_no" id="home_tel_no" required>
+                </br>
 
 
-              <button type="reset" class="btn cancelbtn" class="form_btn" >Cancel</button>
-              <button type="submit" class="btn" class="form_btn" style="margin-right :10px" id="register" disabled>Register</button>
+                <button type="reset" class="btn cancelbtn" class="form_btn" >Cancel</button>
+                <button type="submit" class="btn" class="form_btn" style="margin-right :10px" id="register" disabled>Register</button>
               </div>
               
             </div>
@@ -86,11 +87,11 @@
           </div>
           <hr>
         </div>
-            </form>
-            <!-- include footer seperately -->
-            <?php
-                include("footer.php");
-            ?>
+      </form>
+      <?php
+        // include footer seperately
+        include("footer.php");
+      ?>
           
     </body>
 </html>
