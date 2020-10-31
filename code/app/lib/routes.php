53f374fb765941b2f9608664eeab3afc9fce31ce
Minuri427 <yasarawickramanayaka@gmail.com>
2020-10-31 19:32:28 +0530

    add routes

--- code/app/lib/routes.php
+++ code/app/lib/routes.php
@@ -120,4 +120,16 @@
     Route::set('add_timeslot',function(){
         Home::create_view('add_timeslot');
     });
+
+    Route::set('update_service',function(){
+        Home::create_view('update_service');
+    });
+
+    Route::set('update_delete_service',function(){
+        Home::create_view('update_delete_service');
+    });
+
+    Route::set('update_timeslot',function(){
+        Home::create_view('update_timeslot');
+    });
 ?>
