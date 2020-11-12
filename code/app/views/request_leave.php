c78bb58c3f2842ffb694040e4b4ba916d2d72958
Minuri427 <yasarawickramanayaka@gmail.com>
2020-11-13 00:05:35 +0530

    update add leave view

--- code/app/views/request_leave.php
+++ code/app/views/request_leave.php
@@ -9,6 +9,9 @@
       <link rel="stylesheet" type="text/css" href="public\css\footer.css"><!-- add style to footer -->
       
       <script src="https://kit.fontawesome.com/45c8933d3d.js" crossorigin="anonymous"></script>
+
+      <script type="text/javascript" src="public\js\add_emp_leave.js"></script>
+      <script src="public/js/jquery/jquery.alertable.min.js"></script>
     </head>
 
     <body>
@@ -21,8 +24,8 @@
         <li>Add Leave</li>    
       </ul>
       
-      <form action="./controllers/Register.php">
-        <div class="container">  
+      <form name="add_leave" id="add_leave" method = "post" onsubmit="send_leave_data()">        
+      <div class="container">  
           <h1>Add Leave</h1>
           <hr></br>
           <div class="background">
@@ -33,15 +36,18 @@
                 &ensp;&ensp;<label for="date"><div class="form_label"><b>Select Date</b></div></label>
                 <input type="date" placeholder="Enter the date" name="date" id="date" required></br>
 
-                &ensp;&ensp;<label for="service_type"><div class="form_label"><b>Leave Type</b></div></label>
-                <select name="service_type" id="service_type">
+                &ensp;&ensp;<label for="emp_id"><div class="form_label"><b>Employee Id</b></div></label>
+                <input type="text" placeholder="Enter employee id" name="emp_id" id="emp_id" required></br>
+
+                &ensp;&ensp;<label for="leave_type"><div class="form_label"><b>Leave Type</b></div></label>
+                <select name="leave_type" id="leave_type">
                     <option value="Full_leave">Full Leave</option>
                     <option value="half_day">half Day</option>
                     <option value="short_leave">Short Leave</option>
                 </select><br>
 
-                &ensp;&ensp;<label for="service_type"><div class="form_label"><b>Seect Time</b></div></label>
-                <select name="service_type" id="service_type">
+                &ensp;&ensp;<label for="leave_time"><div class="form_label"><b>Seect Time</b></div></label>
+                <select name="leave_time" id="leave_time">
                   <option value="Full_leave">full day</option>
                   <option value="half_day1">08:00 am - 12:00am</option>
                   <option value="half_day2">12:00 pm - 05:00pm</option>
@@ -49,8 +55,8 @@
                   <option value="short_leave2">03:00 pm - 05:00pm</option>
                 </select><br>
               
-                &ensp;&ensp;<label for="tp_no"><div class="form_label"><b>Reason</b></div></label>
-               <input type="tel" placeholder="Enter your reason here" name="tp_no" id="tp_no" required></br>
+                &ensp;&ensp;<label for="reason"><div class="form_label"><b>Reason</b></div></label>
+               <input type="text" placeholder="Enter your reason here" name="reason" id="reason" required></br>
 
                 <button type="submit" class="btn cancelbtn" class="form_btn" >Cancel</button>
                 <button type="submit" class="btn" class="form_btn" style="margin-right :10px">Submit</button>
