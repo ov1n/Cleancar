a306fbffdcb136d887ca8bd0dbd8dff312c92461
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-11-14 10:10:44 +0530

    add new sidenav_login js

---
+++ code/public/js/sidenav_login.js
@@ -0,0 +1,14 @@
+
+function openSideNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeSideNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+
+
+
