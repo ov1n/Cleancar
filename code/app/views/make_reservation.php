5f4c957c1f7d47dad355b6d5159876931b853884
ov1n <39676588+ov1n@users.noreply.github.com>
2020-11-17 18:42:26 +0530

    Fixed breadcrumb for make reservation customer

--- code/app/views/make_reservation.php
+++ code/app/views/make_reservation.php
@@ -31,7 +31,6 @@
          <img src="public\images\4444.png" width="109" height="48" style="float:right">
         </br>
         <li><a href="home">Home</a></li>
-        <li><a href="receptionist">Receptionist Home</a></li>
         <li>Make Reservation</li>    
       </ul><!-- breadcrumb -->
 
