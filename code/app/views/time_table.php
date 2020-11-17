a942fc27435ddcc5556e6194da8f69c301cad1df
Minuri427 <yasarawickramanayaka@gmail.com>
2020-11-17 20:31:47 +0530

    reset paths

--- code/app/views/time_table.php
+++ code/app/views/time_table.php
@@ -18,7 +18,7 @@
         <img src="public\images\4444.png" width="109" height="48" style="float:right">
         </br>
         <li><a href="home">Home</a></li>
-        <li><a href="home">Manager</a></li>
+        <li><a href="manager">Manager Home</a></li>
         <li>Timetable</li>
     </ul><!-- breadcrumb -->
 <h2>Time Table</h2>
