031f28c27f95a1b0fd2b0d380e152cfeecf6bbdc
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-12-31 23:51:33 +0530

    add 403 error page

---
+++ code/app/views/error-403.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>403 Error</title>
+</head>
+<body>
+<div class="Error403" style="text-align: center;margin-top: 50px">
+	<h1 style="font-size: 45px">Stop !</h1>
+	<h3>403 - This is forbidden area</h3>
+	<p style="font-size: 16px">You do not have permission to view this resource</p>
+	<div class="img" >
+		<img src="public/images/imageedit_1_3680849503.jpg" style="width: 700px;height: 350px;margin: 5%;border-radius: 15px; box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.5);">
+	</div>
+
+	
+</div>
+</body>
+</html>
