e2ebdec80fe00c99e577e34a363974d0fc785dd5
Minuri427 <yasarawickramanayaka@gmail.com>
2021-02-09 15:31:14 +0530

    create js file for add_timeslot

---
+++ code/public/js/add_timeslot.js
@@ -0,0 +1,20 @@
+function send_data() {
+    $.ajax({
+      url: "add_timeslot_",    //the page containing php script
+      type: "post",    //request type,
+      dataType: 'json',
+      //the data array
+      data: {
+        start_time: document.add_timeslot_form.start_time.value,
+        end_time: document.add_timeslot_form.end_time.value,
+        lift_no: document.add_timeslot_form.lift_no.value,
+        
+      }
+      ,
+      //debugging
+      success: function () {
+        console.log("succesfully sent data;");
+      }
+    });
+    //console.log('Registration Successful');
+  }
