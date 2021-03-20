b9c7a5066d453bd9e9dc847491a152aab98a851e
ov1n <thenukaovin@gmail.com>
2021-03-20 15:04:05 +0530

    Finalized table switching in customer calendar

--- code/public/js/calendar/calendar_table.js
+++ code/public/js/calendar/calendar_table.js
@@ -1,3 +1,10 @@
+
+function dasd(){
+    console.log(full_service_slots);
+    console.log(normal_service_slots);
+    console.log(body_wash_slots);
+}
+
 //function to convert data to readable format
 function time_format(value){
     console.log(value);
@@ -5,6 +12,7 @@ function time_format(value){
 
 function display_table(e){
     
+    var days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
     //var selectedType = e.value;
     console.log(e);
 
@@ -13,24 +21,60 @@ function display_table(e){
     z=document.getElementById("body_wash_table");
     
     if(e=="Normal Service"){
-        
 
-        x.style.visibility = "visible";
-        y.style.visibility = "hidden";
-        z.style.visibility = "hidden";
+        create_table(normal_service_slots,days);
     }
     else if(e=="Full Service"){
 
-        x.style.visibility = "hidden";
-        y.style.visibility = "visible";
-        z.style.visibility = "hidden";
+        create_table(full_service_slots,days);
 
     }else if(e=="Body Wash"){
 
-        x.style.visibility = "hidden";
-        y.style.visibility = "hidden";
-        z.style.visibility = "visible";
-
+        create_table(body_wash_slots,days);
     }
     
+}
+
+//days is an array containing dates 
+function create_table(service_type,days){
+
+    document.getElementById("table_create").innerHTML = "";
+
+    var table = document.createElement('table');
+
+    //Add heading rows
+    var trh = document.createElement('tr');
+
+    var time_slots = document.createElement('th');
+    time_slots.appendChild(document.createTextNode('Time'));
+    trh.appendChild(time_slots);
+
+    //add the days HOW TO PROGRESS?
+    for (var i = 0; i <  days.length; i++){
+        var day = document.createElement('th');
+        day.appendChild(document.createTextNode(days[i]));
+        trh.appendChild(day);
+    }
+    table.appendChild(trh);
+
+    for (var i = 0; i <  service_type.length; i++){
+        //console.log(i);
+        var tr = document.createElement('tr');   
+
+        var td1 = document.createElement('td');
+        var text1 = document.createTextNode(service_type[i]);
+        td1.appendChild(text1);
+        tr.appendChild(td1);
+
+        for (var j = 0; j <  days.length; j++){
+
+            var td2 = document.createElement('td');       
+            var text2 = document.createTextNode('Text2');    
+            td2.appendChild(text2);  
+            tr.appendChild(td2);
+        }
+
+        table.appendChild(tr);
+    }
+    table_create.appendChild(table);
 }
