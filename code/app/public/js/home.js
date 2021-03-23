368203697a747c91c4bcdf91424ae8cc38ae0855
Minuri427 <yasarawickramanayaka@gmail.com>
2021-03-23 12:20:09 +0530

    delete unnecessary folder

--- code/app/public/js/home.js
+++
@@ -1,7 +0,0 @@
-//function which shows popup on click
-function reservation_click(){   
-    //alert("Make reservation on the below entered details?");
-    console.log("function initialised");
-    $.alertable.alert('Please login to make reservation').always(function() {
-    });
-}
