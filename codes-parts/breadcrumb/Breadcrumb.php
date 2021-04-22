9158d2c20b747403479f958570f74b7083be5dac
ov1n <thenukaovin@gmail.com>
2021-04-23 01:37:00 +0530

    Normalized diagram structures

--- codes-parts/breadcrumb/Breadcrumb.php
+++
@@ -1,8 +0,0 @@
-<ul class="breadcrumb">
-  <img src="../../public/images/4444.png" width="109" height="48" style="float:right">
-  </br>
-  <li><a href="home">Home</a></li>
-  <li><a href="home">Manager</a></li>
-  <li>Add Service</li>
-  
-</ul>
